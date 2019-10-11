c1faad8fa8f6e15e993f7ce63416ecf42992d550
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-10-11 18:08:11 +0800

    Documentation update

--- output.tf
+++ output.tf
@@ -1,19 +1,24 @@
 output "object" {
+  description = "Output the full object"
   value = azurerm_public_ip.public_ip
 }
 
 output "ip_address" {
+  description = "Output the ip address"
   value = azurerm_public_ip.public_ip.ip_address
 }
 
 output "fqdn" {
+  description = "Output the fully qualified domain name"
   value = azurerm_public_ip.public_ip.fqdn
 }
 
 output "name" {
+  description = "Output the object name"
   value = azurerm_public_ip.public_ip.name
 }
 
 output "id" {
+  description = "Output the object ID"
   value = azurerm_public_ip.public_ip.id
 }

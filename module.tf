c2d24da4d9af4eee1aaf36140f4f0d770e3db245
Arnaud Lheureux <arnaudlh@hotmail.com>
2019-11-22 08:02:55 +0800

    Refactor towards lookup AB#41

--- module.tf
+++ module.tf
@@ -5,11 +5,11 @@ resource "azurerm_public_ip" "public_ip" {
   allocation_method         = var.ip_addr.allocation_method
   tags                      = local.tags
 
-  sku                       = "${contains(keys(var.ip_addr), "sku") ? var.ip_addr.sku : null}"
-  ip_version                = "${contains(keys(var.ip_addr), "ip_version") ? var.ip_addr.ip_version : null}"  
-  domain_name_label         = "${contains(keys(var.ip_addr), "dns_prefix") ? var.ip_addr.dns_prefix : null}"
-  idle_timeout_in_minutes   = "${contains(keys(var.ip_addr), "timeout") ? var.ip_addr.timeout : null}"
-  zones                     = "${contains(keys(var.ip_addr), "zones") ? var.ip_addr.zones : null}"
-  reverse_fqdn              = "${contains(keys(var.ip_addr), "reverse_fqdn") ? var.ip_addr.reverse_fqdn : null}"
-  public_ip_prefix_id       = "${contains(keys(var.ip_addr), "public_ip_prefix_id") ? var.ip_addr.public_ip_prefix_id : null}"
+  sku                       = lookup(var.ip_addr, "sku"), null)
+  ip_version                = lookup(var.ip_addr, "ip_version", null)   
+  domain_name_label         = lookup(var.ip_addr, "dns_prefix", null) 
+  idle_timeout_in_minutes   = lookup(var.ip_addr, "timeout", null) 
+  zones                     = lookup(var.ip_addr, "zones", null) 
+  reverse_fqdn              = lookup(var.ip_addr, "reverse_fqdn", null)
+  public_ip_prefix_id       = lookup(var.ip_addr, "public_ip_prefix_id", null)
 }
